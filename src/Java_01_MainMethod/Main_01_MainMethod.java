8f25a24ebefa57747b0034cb4025a1f919c0d441
aybüke <aybukeegrbzz@gmail.com>
2022-11-07 14:46:03 +0300

    07/11

---
+++ src/Java_01_MainMethod/Main_01_MainMethod.java
@@ -0,0 +1,17 @@
+package Java_01_MainMethod;
+
+public class Main_01_MainMethod {
+    public static void main(String[] args) {
+
+		/*
+		 *  Public is access modifier                 --> Genel erişim değiştiricisidir
+            Static is non-access modifier             --> Statik, erişim dışı değiştiricidir.
+			Void is return type. (Returns nothing).   --> Void dönüş türüdür. (Hiçbir şey döndürmez).
+			Main is method name.                      --> Ana yöntem adıdır.
+		    Java main method accepts a single argument of type String array.
+		         --> Java ana yöntemi, String dizisi türünde tek bir argüman kabul eder.
+			This is also called as java command line arguments.
+			     --> Bu aynı zamanda Java komut satırı argümanları olarak da adlandırılır.
+		 */
+    }
+}
